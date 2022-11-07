0283eff5c6b80a3ca34c89d4ff515df7b0e67cff
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-11-07 15:36:08 +0100

    refactor tests

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -5,25 +5,26 @@ import NumberOfEvents from '../NumberOfEvents';
 
 describe('<NumberOfEvents /> component', () => {
 
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
+  })
+
   test('render number input', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     expect(NumberOfEventsWrapper.find('.number-input')).toHaveLength(1);
   });
 
   test('renders number input correctly', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
     expect(NumberOfEventsWrapper.find('.number-input').prop('value')).toBe(numberOfEvents);
   });
 
   test('show default number of 32', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     const numberOfEvents = 32;
     expect(NumberOfEventsWrapper.find('.number-input').prop('value')).toBe(numberOfEvents);
   });
 
   test('change state when number input changes', () => {
-    const NumberOfEventsWrapper = shallow(<NumberOfEvents />);
     const eventObject = { target: { value: 16 } };
     NumberOfEventsWrapper.find('.number-input').simulate('change', eventObject);
     expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(16);
