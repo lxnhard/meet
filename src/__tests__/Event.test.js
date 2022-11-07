a8826356b06f30edf39799e1ba16058daa28d757
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-11-07 13:57:13 +0100

    refactor tests

--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -7,30 +7,30 @@ import { mockData } from '../mock-data';
 
 describe('<Event /> component', () => {
 
+  let EventWrapper;
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={mockData[0]} />);
+  });
+
   test('render event overview on default', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find('.event-overview')).toHaveLength(1);
   });
 
   test('render start time, summary in overview', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find('.start-time')).toHaveLength(1);
     expect(EventWrapper.find('.summary')).toHaveLength(1);
   });
 
   test('render correct start time, summary in overview', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find('.start-time').text()).toBe(mockData[0].start.dateTime);
     expect(EventWrapper.find('.summary').text()).toBe(mockData[0].summary);
   });
 
   test('render details button on default', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     expect(EventWrapper.find('.btn-details')).toHaveLength(1);
   });
 
   test('change state when button clicked', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     EventWrapper.setState({
       isExpanded: false
     });
@@ -39,15 +39,17 @@ describe('<Event /> component', () => {
   });
 
   test('show detailed view instead of overview after clicking expand button', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
-    EventWrapper.find('.btn-details').simulate('click');
+    EventWrapper.setState({
+      isExpanded: true
+    });
     expect(EventWrapper.find('.event-details')).toHaveLength(1);
     expect(EventWrapper.find('.event-overview')).toHaveLength(0);
   });
 
   test('render summary, start-time, end-time, description, location in detailed view', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
-    EventWrapper.find('.btn-details').simulate('click');
+    EventWrapper.setState({
+      isExpanded: true
+    });
     expect(EventWrapper.find('.summary')).toHaveLength(1);
     expect(EventWrapper.find('.start-time')).toHaveLength(1);
     expect(EventWrapper.find('.end-time')).toHaveLength(1);
@@ -56,8 +58,9 @@ describe('<Event /> component', () => {
   });
 
   test('render correct summary, start-time, end-time, description, location in detailed view', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
-    EventWrapper.find('.btn-details').simulate('click');
+    EventWrapper.setState({
+      isExpanded: true
+    });
     expect(EventWrapper.find('.summary').text()).toBe(mockData[0].summary);
     expect(EventWrapper.find('.start-time').text()).toBe(mockData[0].start.dateTime);
     expect(EventWrapper.find('.end-time').text()).toBe(mockData[0].end.dateTime);
@@ -66,13 +69,13 @@ describe('<Event /> component', () => {
   });
 
   test('render collapse button in detailed view', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
-    EventWrapper.find('.btn-details').simulate('click');
+    EventWrapper.setState({
+      isExpanded: true
+    });
     expect(EventWrapper.find('.btn-collapse')).toHaveLength(1);
   });
 
   test('change state when collapse button clicked', () => {
-    const EventWrapper = shallow(<Event event={mockData[0]} />);
     EventWrapper.setState({
       isExpanded: true
     });
