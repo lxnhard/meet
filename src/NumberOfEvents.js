6f056250da9ac59f5c09bac36c600cd80ac6244a
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-11-27 17:21:25 +0100

    change event number options + style

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -33,11 +33,10 @@ class NumberOfEvents extends Component {
           value={this.state.eventCountInput}
           onChange={this.handleNumber}
         >
-          <option value="1">1</option>
-          <option value="25">25</option>
+          <option value="10">10</option>
+          <option value="20">20</option>
+          <option value="30">30</option>
           <option value="50">50</option>
-          <option value="75">75</option>
-          <option value="100">100</option>
         </select>
       </div>
     )
