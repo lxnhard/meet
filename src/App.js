0180c83be8e11d1a4d57d76053cbe2002b4efc43
lxnhard <106260289+lxnhard@users.noreply.github.com>
2022-12-03 09:51:09 +0100

    implement chart components with swap button

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import Paginator from './Paginator';
-import Charts from './Charts';
+import ChartCity from './ChartCity';
 import { WarningAlert } from './Alert'
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
+import ChartEventGenre from './ChartEventGenre';
 
 class App extends Component {
 
@@ -20,7 +21,8 @@ class App extends Component {
     eventsTotalCount: null,
     warningText: '',
     data: [],
-    query: ''
+    query: '',
+    showChart: 'bar'
   }
 
   updateEvents = (location, eventCount, newPage) => {
@@ -80,6 +82,13 @@ class App extends Component {
     this.setState({ query });
   }
 
+  swapChart = () => {
+    this.setState(
+      { showChart: (this.state.showChart === 'bar' ? 'pie' : 'bar') }
+    );
+    console.log(this.state.showChart)
+  }
+
   async componentDidMount() {
     this.mounted = true;
     this.checkOffline();
@@ -108,7 +117,24 @@ class App extends Component {
           <CitySearch locations={this.state.locations} location={this.state.location} updateEvents={this.updateEvents} query={this.state.query} handleQueryChange={this.handleQueryChange} />
           <NumberOfEvents updateEvents={this.updateEvents} page={this.state.page} />
         </div>
-        {this.state.data.length > 0 && <Charts data={this.state.data} updateEvents={this.updateEvents} locations={this.state.locations} handleQueryChange={this.handleQueryChange} />}
+        <div className="btn-chart-container"><button className='btn-chart' onClick={this.swapChart}
+          style={this.state.showChart === 'bar' ?
+            {
+              backgroundImage: "url(/meet/icon-pie_chart.svg)",
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
+              backgroundSize: '80%'
+            } :
+            {
+              backgroundImage: "url(/meet/icon-bar_chart.svg)",
+              backgroundRepeat: 'no-repeat',
+              backgroundPosition: 'center center',
+              backgroundSize: '80%'
+            }}
+          title={this.state.showChart === 'bar' ? "Show event genre distribution" : "Show event city distribution"}
+        ></button></div>
+        {this.state.showChart === 'bar' && this.state.data.length > 0 && <ChartCity data={this.state.data} updateEvents={this.updateEvents} locations={this.state.locations} handleQueryChange={this.handleQueryChange} />}
+        {this.state.showChart === 'pie' && <ChartEventGenre events={this.state.events} />}
         <EventList events={this.state.events} />
         <Paginator page={this.state.page} updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} eventsTotalCount={this.state.eventsTotalCount} />
       </div>
